f6034f9f663f4a7c6888d7bf41f240c5995333d4
Artem Yevtushenko <ebtyx2809@gmail.com>
2025-08-23 00:30:33 +0300

    Created first use cases.

---
+++ Internet_shop.Application/Contracts/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using Internet_shop.Domain.Models;
+
+namespace Internet_shop.Application.Contracts;
+
+public interface ICategoryRepository
+{
+    Task CreateCategoryAsync(Category category);
+    Task UpdateCategoryAsync(Category category);
+    Task DeleteCategoryAsync(Guid categoryId);
+    Task<Category?> GetCategoryByIdAsync(Guid categoryId);
+    Task<IReadOnlyCollection<Category>> GetAllCategoriesAsync();
+}
---
+++ Internet_shop.Application/Contracts/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using Internet_shop.Domain.Models;
+
+namespace Internet_shop.Application.Contracts;
+
+public interface IUserRepository
+{
+    Task CreateUserAsync(User user);
+    Task UpdateUserAsync(User user);
+    Task DeleteUserAsync(Guid userId);
+    Task<User?> GetUserById(Guid userId);    
+}
