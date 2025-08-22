40ff524c9c07d11b94000868e4bb2b5d047eaa54
Artem Yevtushenko <ebtyx2809@gmail.com>
2025-08-23 00:30:33 +0300

    Created first use cases.

---
+++ Internet_shop.Application/Contracts/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using Internet_shop.Domain.Models;
+
+namespace Internet_shop.Application.Contracts;
+
+public interface IProductRepository
+{
+    Task CreateProductAsync(Product product);
+    Task UpdateProductAsync(Product product);
+    Task DeleteProductAsync(Guid productId);
+    Task<Product?> GetProductByIdAsync(Guid productId);
+    Task<IReadOnlyCollection<Product>> GetAllProductsAsync(); // Implement pagination and sorting here
+}
