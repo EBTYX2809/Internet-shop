829364e293c439901660175ba7a847bbbe2fe023
EBTYX2809 <ebtyx2809@gmail.com>
2025-11-26 03:43:31 +0200

    Created DbContext and DI

---
+++ Internet_shop.Infrastructure/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Internet_shop.Domain.Models;
+using Internet_shop.Infrastructure.Configurations;
+using Internet_shop.Infrastructure.Entities;
+using Internet_shop.Infrastructure.EntityConfigurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Internet_shop.Infrastructure;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<ProductReview> ProductReviews { get; set; }
+    public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+    public DbSet<Order> Orders { get; set; }    
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {        
+        modelBuilder.ApplyConfiguration<UserEntity>(new UserEntityConfiguration());
+        modelBuilder.ApplyConfiguration<ProductEntity>(new ProductEntityConfiguration());
+        modelBuilder.ApplyConfiguration<ProductReviewEntity>(new ProductReviewEntityConfiguration());
+        modelBuilder.ApplyConfiguration<ShoppingCartItemEntity>(new ShoppingCartItemEntityConfiguration());
+        modelBuilder.ApplyConfiguration<OrderEntity>(new OrderEntityConfiguration());
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
