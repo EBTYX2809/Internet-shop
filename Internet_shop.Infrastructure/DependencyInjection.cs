0deec3cffee22855a6da37a03027703901bc9d9d
EBTYX2809 <ebtyx2809@gmail.com>
2025-11-26 03:43:31 +0200

    Created DbContext and DI

---
+++ Internet_shop.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Internet_shop.Application.Contracts;
+using Internet_shop.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Internet_shop.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseNpgsql(
+                configuration.GetConnectionString("DefaultConnection")));
+        
+        services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<ISearchProductRepository, SearchProductRepository>();
+
+        return services;
+    }
+}
