acdb3bf3b10fbe4ac643dd42e1c282850629a707
EBTYX2809 <ebtyx2809@gmail.com>
2025-12-27 08:57:28 +0200

    Took out ProductInfo into separate class.

--- Internet_shop.Domain/Models/Product.cs
+++ Internet_shop.Domain/Models/Product.cs
@@ -3,11 +3,9 @@
 namespace Internet_shop.Domain.Models;
 
 public class Product
-{
-    public Guid Id { get; init; }
-    public string Name { get; private set; }
+{ 
+    public ProductInfo ProductInfo { get; private set; }
     public string Description { get; private set; }
-    public decimal Price { get; private set; }
     public CategoryInfo Category { get; private set; }
     public CategoryInfo SubCategory { get; private set; }
     private Dictionary<string, string> _specificationList;
@@ -16,65 +14,23 @@ public class Product
     public IReadOnlyCollection<ProductReview> Reviews => _reviews.AsReadOnly();
     public double Rating { get; private set; }
 
-    public Product(string name, string description, decimal price,
-        CategoryInfo category, CategoryInfo subCategory, Dictionary<string, string> specificationList)
+    public Product(ProductInfo productInfo, string description, CategoryInfo category, 
+        CategoryInfo subCategory, Dictionary<string, string> specificationList)
     {
-        Id = Guid.NewGuid();
-
-        if (string.IsNullOrWhiteSpace(name) || name.Length > 20)
-            throw new ArgumentException("Name can't be null or longer than 20 symbols.");
+        ProductInfo = productInfo;
 
         if (string.IsNullOrWhiteSpace(description) || description.Length > 100)
             throw new ArgumentException("Description can't be null or longer than 100 symbols.");
 
-        if (price <= 0)
-            throw new ArgumentException("Price can't be lower or equal to 0.");
-
         ArgumentNullException.ThrowIfNull(category, nameof(category));
 
-        Name = name;
         Description = description;
-        Price = price;
         Category = category;
         _specificationList = specificationList;
         SubCategory = subCategory;
 
         _reviews = new List<ProductReview>();
-    }
-
-    public Product(Guid id, string name, string description, decimal price,
-        CategoryInfo category, CategoryInfo subCategory, Dictionary<string, string> specificationList)
-    {
-        Id = id;
-
-        if (string.IsNullOrWhiteSpace(name) || name.Length > 20)
-            throw new ArgumentException("Name can't be null or longer than 20 symbols.");
-
-        if (string.IsNullOrWhiteSpace(description) || description.Length > 100)
-            throw new ArgumentException("Description can't be null or longer than 100 symbols.");
-
-        if (price <= 0)
-            throw new ArgumentException("Price can't be lower or equal to 0.");
-
-        ArgumentNullException.ThrowIfNull(category, nameof(category));
-
-        Name = name;
-        Description = description;
-        Price = price;
-        Category = category;
-        _specificationList = specificationList;
-        SubCategory = subCategory;
-
-        _reviews = new List<ProductReview>();
-    }
-
-    public void Rename(string name)
-    {
-        if (string.IsNullOrWhiteSpace(name) || name.Length > 20)
-            throw new ArgumentException("Name can't be null or longer than 20 symbols.");
-
-        Name = name;
-    }
+    }    
 
     public void ChangeDescription(string description)
     {
@@ -82,14 +38,6 @@ public class Product
             throw new ArgumentException("Description can't be null or longer than 100 symbols.");
 
         Description = description;
-    }
-
-    public void ChangePrice(decimal price)
-    {
-        if (price <= 0)
-            throw new ArgumentException("Price can't be lower or equal to 0.");
-
-        Price = price;
     }
 
     public void ChangeCategory(CategoryInfo categoryInfo)
---
+++ Internet_shop.Domain/Models/ProductInfo.cs
@@ -0,0 +1,52 @@
+﻿namespace Internet_shop.Domain.Models;
+
+public class ProductInfo
+{
+    public Guid Id { get; init; }    
+    public string Name { get; private set; }
+    public decimal Price { get; private set; }
+
+    public ProductInfo(Guid id, string name, decimal price)
+    {
+        Id = id;
+
+        if (string.IsNullOrWhiteSpace(name) || name.Length > 20)
+            throw new ArgumentException("Name can't be null or longer than 20 symbols.");
+
+        if (price <= 0)
+            throw new ArgumentException("Price can't be lower or equal to 0.");
+
+        Name = name;
+        Price = price;
+    }
+
+    public ProductInfo(string name, decimal price)
+    {
+        Id = Guid.NewGuid();
+
+        if (string.IsNullOrWhiteSpace(name) || name.Length > 20)
+            throw new ArgumentException("Name can't be null or longer than 20 symbols.");
+
+        if (price <= 0)
+            throw new ArgumentException("Price can't be lower or equal to 0.");
+
+        Name = name;
+        Price = price;
+    }
+
+    public void Rename(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name) || name.Length > 20)
+            throw new ArgumentException("Name can't be null or longer than 20 symbols.");
+
+        Name = name;
+    }
+
+    public void ChangePrice(decimal price)
+    {
+        if (price <= 0)
+            throw new ArgumentException("Price can't be lower or equal to 0.");
+
+        Price = price;
+    }
+}
