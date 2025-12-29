dc9fb7e77807b7d88eb5f29a77165fef645b2c52
EBTYX2809 <ebtyx2809@gmail.com>
2025-12-29 16:22:32 +0200

    Moved all user repositories to separate directory.

---
+++ Internet_shop.Application/Contracts/UserContracts/IGoogleUserDataRepository.cs
@@ -0,0 +1,10 @@
+﻿using Internet_shop.Domain.Models.UserData;
+
+namespace Internet_shop.Application.Contracts.UserContracts;
+
+public interface IGoogleUserDataRepository
+{
+    Task AddGoogleIdAsync(GoogleUserData data);
+    Task<Customer?> GetCustomerByGoogleIdAsync(string googleId);
+    Task DeleteGoogleUserDataAsync(GoogleUserData data);
+}
---
+++ Internet_shop.Application/Contracts/UserContracts/IRefreshTokenRepository.cs
@@ -0,0 +1,11 @@
+﻿using Internet_shop.Domain.Models.UserData;
+
+namespace Internet_shop.Application.Contracts.UserContracts;
+
+public interface IRefreshTokenRepository
+{
+    Task AddTokenAsync(RefreshToken token);
+    Task<RefreshToken?> GetRefreshTokenAsync(Guid id);
+    Task RevokeTokenAsync(Guid id);
+    Task DeleteTokenAsync(RefreshToken token);
+}
